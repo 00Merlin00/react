e1044fbae6503dd07f9ec2de7b59ed4549710508
Mohit Rautela <mrautela12@gmail.com>
2022-12-10 01:33:56 +0530

    some commit

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,14 +1,26 @@
 import logo from "./logo.svg";
 import "./App.css";
-import User from "./User";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
-      <User />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
 
 export default App;
+aaaaa;
