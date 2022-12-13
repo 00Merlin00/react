7a687678bbefe90c1a01779d9c78ba03903478a1
Mohit Rautela <mrautela12@gmail.com>
2022-12-13 18:57:00 +0530

    some commit

---
+++ blog/src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+import React, { useState } from "react";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { data: "mohit" };
+  }
+  apple() {
+    this.setState({ data: "rautela" });
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>{this.state.data} </h1>
+        <button
+          onClick={() => {
+            this.apple();
+          }}
+        >
+          Update Data
+        </button>
+      </div>
+    );
+  }
+}
+
+export default App;
