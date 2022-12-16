d114855e66746db1d3216491529a603569350421
Mohit Rautela <mrautela12@gmail.com>
2022-12-17 01:29:10 +0530

    some commit

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,16 +1,29 @@
 import "./App.css";
 import Student from "./Student";
-import React from "react";
+import React, { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Props</h1>
-      <Student name="Mohit" email="mohit@test.com">
-        {" "}
-      </Student>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "Mohit",
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Props</h1>
+        <Student name={this.state.name} email="mohit@test.com"></Student>
+        <button
+          onClick={() => {
+            this.setState({ name: "Marry" });
+          }}
+        >
+          Update Info
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
