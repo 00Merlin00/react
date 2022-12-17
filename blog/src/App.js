9cb91f42c250c55f9257416077871a64a0afb723
Mohit Rautela <mrautela12@gmail.com>
2022-12-18 02:41:49 +0530

    some commit

--- blog/src/App.js
+++ blog/src/App.js
@@ -4,18 +4,23 @@ import React, { useState } from "react";
 
 function App() {
   let [data, setData] = useState(null);
-  let { print, setPrint } = useState(false);
+  let [print, setPrint] = useState(false);
 
   function getData(val) {
-    console.log(val.target.value);
     setData(val.target.value);
   }
+
   return (
     <div className="App">
-      <h1>Get Input box value !</h1>
       {print ? <h1>{data}</h1> : null}
-      <input type="text" onChange={getData}></input>
-      <button onClick={setPrint(true)}>click Update</button>
+      <input type={"text"} onChange={getData}></input>
+      <button
+        onClick={() => {
+          setPrint(true);
+        }}
+      >
+        click to Update
+      </button>
     </div>
   );
 }
