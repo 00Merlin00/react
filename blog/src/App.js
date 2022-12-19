02b112f2c7c1ca5b838602d06059ae64403055f9
Mohit Rautela <mrautela12@gmail.com>
2022-12-20 01:49:53 +0530

    some commit

--- blog/src/App.js
+++ blog/src/App.js
@@ -7,20 +7,38 @@ function App() {
   let [tnc, setTnc] = useState(false);
   let [interest, setInterest] = useState(null);
   function getFormData(e) {
+    console.log(name, tnc, interest);
     e.preventDefault();
   }
   return (
     <div className="App">
       <h1>Handle form in React</h1>
       <form onSubmit={getFormData}>
-        <input type={"text"} placeholder="type name"></input> <br /> <br />
-        <select>
+        <input
+          type={"text"}
+          placeholder="type name"
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        ></input>{" "}
+        <br /> <br />
+        <select
+          onChange={(e) => {
+            setInterest(e.target.value);
+          }}
+        >
           <option>Select Any</option>
           <option>DBZ</option>
           <option>Naruto</option>
         </select>{" "}
         <br /> <br />
-        <input type={"checkbox"}></input> <span>Accept Term & Conditions</span>
+        <input
+          onChange={(e) => {
+            setTnc(e.target.checked);
+          }}
+          type={"checkbox"}
+        ></input>{" "}
+        <span>Accept Term & Conditions</span>
         <br /> <br />
         <button type="submit">Submit</button>
       </form>
