44668c4826773316998d92889c175e148738f934
Mohit Rautela <mrautela12@gmail.com>
2022-12-20 01:39:48 +0530

    some commit

--- blog/src/App.js
+++ blog/src/App.js
@@ -3,9 +3,27 @@ import "./App.css";
 import React, { useState } from "react";
 
 function App() {
+  let [name, setName] = useState(null);
+  let [tnc, setTnc] = useState(false);
+  let [interest, setInterest] = useState(null);
+  function getFormData(e) {
+    e.preventDefault();
+  }
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <h1>Handle form in React</h1>
+      <form onSubmit={getFormData}>
+        <input type={"text"} placeholder="type name"></input> <br /> <br />
+        <select>
+          <option>Select Any</option>
+          <option>DBZ</option>
+          <option>Naruto</option>
+        </select>{" "}
+        <br /> <br />
+        <input type={"checkbox"}></input> <span>Accept Term & Conditions</span>
+        <br /> <br />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
