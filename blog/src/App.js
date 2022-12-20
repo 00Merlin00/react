89f2d6614713ad24c8cf089e08242153049c1a33
Mohit Rautela <mrautela12@gmail.com>
2022-12-21 00:45:14 +0530

    some commit

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,49 +1,13 @@
 import "./App.css";
 // import Student from "./Student";
+import Profile from "./Profile";
 import React, { useState } from "react";
 
 function App() {
-  let [name, setName] = useState(null);
-  let [tnc, setTnc] = useState(false);
-  let [interest, setInterest] = useState(null);
-  function getFormData(e) {
-    console.log(name, tnc, interest);
-    e.preventDefault();
-  }
+ 
   return (
-    <div className="App">
-      <h1>Handle form in React</h1>
-      <form onSubmit={getFormData}>
-        <input
-          type={"text"}
-          placeholder="type name"
-          onChange={(e) => {
-            setName(e.target.value);
-          }}
-        ></input>{" "}
-        <br /> <br />
-        <select
-          onChange={(e) => {
-            setInterest(e.target.value);
-          }}
-        >
-          <option>Select Any</option>
-          <option>DBZ</option>
-          <option>Naruto</option>
-        </select>{" "}
-        <br /> <br />
-        <input
-          onChange={(e) => {
-            setTnc(e.target.checked);
-          }}
-          type={"checkbox"}
-        ></input>{" "}
-        <span>Accept Term & Conditions</span>
-        <br /> <br />
-        <button type="submit">Submit</button>
-      </form>
-    </div>
-  );
+    )
+   
 }
 
 export default App;
---
+++ blog/src/Profile.js
@@ -0,0 +1,9 @@
+function Profile() {
+  return (
+    <div>
+      <h1>Profile Component</h1>
+    </div>
+  );
+}
+
+export default Profile;
