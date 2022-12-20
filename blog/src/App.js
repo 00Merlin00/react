ee55862d99d42e83f654e12be705f29cb7dc741d
Mohit Rautela <mrautela12@gmail.com>
2022-12-21 00:58:20 +0530

    some commit

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,13 +1,21 @@
 import "./App.css";
-// import Student from "./Student";
 import Profile from "./Profile";
 import React, { useState } from "react";
 
 function App() {
- 
+  let [loggedIn, setLoggedIn] = useState(null);
   return (
-    )
-   
+    <div className="App">
+      {loggedIn == 1 ? (
+        <h1>HELLO USER 1</h1>
+      ) : loggedIn == 2 ? (
+        <h1>HELLO USER 2</h1>
+      ) : (
+        <h1>HELLO USER 3</h1>
+      )}
+      <Profile> </Profile>
+    </div>
+  );
 }
 
 export default App;
