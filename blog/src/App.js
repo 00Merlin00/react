065e0c79ea1e351ce44f99dfbf33eee279160650
Mohit Rautela <mrautela12@gmail.com>
2022-12-26 00:21:43 +0530

    some commit

--- blog/src/App.js
+++ blog/src/App.js
@@ -4,28 +4,15 @@ import React, { useState } from "react";
 class App extends React.Component {
   constructor() {
     super();
-    console.warn("Constructor");
+
     this.state = {
       counter: 0,
     };
   }
-
-  componentDidUpdate(preProp, preState, snapshot) {
-    console.warn("CDU", preState);
-  }
   render() {
-    console.warn("render");
-
     return (
       <div className="App">
-        <h1>Component Did Mount {this.state.counter}</h1>
-        <button
-          onClick={() => {
-            this.setState({ counter: this.state.counter + 1 });
-          }}
-        >
-          Click to Change dox
-        </button>
+        <h1>Should component Update {this.state.counter}</h1>
       </div>
     );
   }
