4278893ca4ba103c73e29feb21b2fe5aa4c93114
Mohit Rautela <mrautela12@gmail.com>
2022-12-30 20:22:45 +0530

    some commit

--- blog/src/App.js
+++ blog/src/App.js
@@ -2,33 +2,10 @@ import "./App.css";
 import React, { useState } from "react";
 
 class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      counter: 0,
-    };
-  }
-
-  shouldComponentUpdate() {
-    console.warn("shouldComponentUpdate", this.state.counter);
-    if (this.state.counter < 9) {
-      return true;
-    } else {
-      return false;
-    }
-  }
   render() {
     return (
       <div className="App">
-        <h1>Should component Update {this.state.counter}</h1>
-        <button
-          onClick={() => {
-            this.setState({ counter: this.state.counter + 1 });
-          }}
-        >
-          Update Counter
-        </button>
+        <h1>Component Will Unmount</h1>
       </div>
     );
   }
