9d31924808627f09dbadf3ec3dbf2c1b8b5fadfd
Mohit Rautela <mrautela12@gmail.com>
2023-01-01 00:11:15 +0530

    some commit

--- blog/src/App.js
+++ blog/src/App.js
@@ -2,12 +2,20 @@ import "./App.css";
 import React, { useEffect, useState } from "react";
 
 function App() {
+  let [count, setCount] = useState(0);
   useEffect(() => {
     console.log("useEffect");
   });
   return (
     <div className="App">
-      <h1>UseEffect in React</h1>
+      <h1>UseEffect in React {count}</h1>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Change
+      </button>
     </div>
   );
 }
