3d58295b2e6604eb22fe787d0c48dc14bd5e6350
Mohit Rautela <mrautela12@gmail.com>
2023-01-03 17:07:15 +0530

    some commit

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,13 +1,28 @@
 import "./App.css";
 import React, { useEffect, useState } from "react";
+import User from "./User";
 
 function App() {
-  useEffect(() => {
-    console.warn("useEffect called");
-  });
+  let [data, setData] = useState(10);
+  let [count, setCount] = useState(100);
+
   return (
     <div className="App">
-      <button>Update Counter</button>
+      <User count={count} data={data} />
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Update Counter
+      </button>
+      <button
+        onClick={() => {
+          setData(data + 1);
+        }}
+      >
+        Update Data
+      </button>
     </div>
   );
 }
--- blog/src/User.js
+++ blog/src/User.js
@@ -1,14 +1,15 @@
-import React, { useEffect } from "react";
+import "./App.css";
+import React, { useEffect, useState } from "react";
 
 function User(props) {
   useEffect(() => {
-    console.log("useEffect");
-  }, []);
+    alert("count. is " + props.count);
+  }, [props.count]);
 
   return (
     <div className="App">
-      {" "}
-      <h1>User Component {props.countProps}</h1>
+      <h1>Count Props : {props.count}</h1>
+      <h1>data Props: {props.data}</h1>
     </div>
   );
 }
