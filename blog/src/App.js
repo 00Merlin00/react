39a67b7a8ebd1fb792e099da90124f9105a8ae41
Mohit Rautela <mrautela12@gmail.com>
2023-01-03 21:24:37 +0530

    some commit

--- blog/src/App.js
+++ blog/src/App.js
@@ -3,26 +3,9 @@ import React, { useEffect, useState } from "react";
 import User from "./User";
 
 function App() {
-  let [data, setData] = useState(10);
-  let [count, setCount] = useState(100);
-
   return (
     <div className="App">
-      <User count={count} data={data} />
-      <button
-        onClick={() => {
-          setCount(count + 1);
-        }}
-      >
-        Update Counter
-      </button>
-      <button
-        onClick={() => {
-          setData(data + 1);
-        }}
-      >
-        Update Data
-      </button>
+      <h1>Style in React js</h1>
     </div>
   );
 }
