b42c84612c49bd84fcac2bd6fb86f014075bf87e
Mohit Rautela <mrautela12@gmail.com>
2023-01-04 18:33:05 +0530

    some commit

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,11 +1,33 @@
 import "./App.css";
-import React, { useEffect, useState } from "react";
-import User from "./User";
+import "./Style.css";
+import { Table } from "react-bootstrap/Table";
 
 function App() {
+  let users = [
+    { name: "mohit", email: "mohit@test.com", contact: 111 },
+    { name: "anil", email: "anil@test.com", contact: 222 },
+    { name: "james", email: "james@test.com", contact: 333 },
+    { name: "kriti", email: "kriti@test.com", contact: 444 },
+  ];
   return (
     <div className="App">
-      <h1>Style in React js</h1>
+      <h1>List with Bootstrap Table</h1>
+      <table>
+        <tbody>
+          <tr>
+            <td>Name</td>
+            <td>E-mail</td>
+            <td>Contact</td>
+          </tr>
+          {users.map((data, k) => (
+            <tr key={k}>
+              <td>{data.name}</td>
+              <td>{data.email}</td>
+              <td>{data.contact}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
