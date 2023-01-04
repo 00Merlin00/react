eb65353d5ca8059fad395ac5b32261120d37b901
Mohit Rautela <mrautela12@gmail.com>
2023-01-05 02:45:00 +0530

    some commit

--- blog/src/App.js
+++ blog/src/App.js
@@ -5,28 +5,30 @@ import Table from "react-bootstrap/Table";
 
 function App() {
   let users = [
-    { name: "mohit", email: "mohit@test.com", contact: 111 },
-    { name: "anil", email: "anil@test.com", contact: 222 },
-    { name: "james", email: "james@test.com", contact: 333 },
-    { name: "kriti", email: "kriti@test.com", contact: 444 },
+    { name: "mohit", email: "mohit@test.com", contact: "111" },
+    { name: "anil", email: "anil@test.com", contact: "222" },
+    { name: "james", email: "james@test.com", contact: "111" },
+    { name: "kriti", email: "kriti@test.com", contact: "444" },
   ];
   return (
     <div className="App">
       <h1>List with Bootstrap Table</h1>
-      <Table style={{ border: "1px solid black" }}>
+      <Table striped variant="dark">
         <tbody>
           <tr>
             <td>Name</td>
             <td>E-mail</td>
             <td>Contact</td>
           </tr>
-          {users.map((data, k) => (
-            <tr key={k}>
-              <td>{data.name}</td>
-              <td>{data.email}</td>
-              <td>{data.contact}</td>
-            </tr>
-          ))}
+          {users.map((data, k) =>
+            data.contact === "111" ? (
+              <tr key={k}>
+                <td>{data.name}</td>
+                <td>{data.email}</td>
+                <td>{data.contact}</td>
+              </tr>
+            ) : null
+          )}
         </tbody>
       </Table>
     </div>
