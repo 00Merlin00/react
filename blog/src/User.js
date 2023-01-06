c4238c63359318ad9cb6dd104f39348c2ee6d928
Mohit Rautela <mrautela12@gmail.com>
2023-01-07 02:11:48 +0530

    some commit

--- blog/src/User.js
+++ blog/src/User.js
@@ -4,6 +4,8 @@ function User(props) {
   return (
     <div>
       <span>{props.item.name}</span>
+      <span>{props.item.email}</span>
+      <span>{props.item.contact}</span>
     </div>
   );
 }
