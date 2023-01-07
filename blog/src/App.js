1c1df80b3880e22df8ae60248369dece0c960666
Mohit Rautela <mrautela12@gmail.com>
2023-01-08 01:57:33 +0530

    some commit

--- blog/src/App.js
+++ blog/src/App.js
@@ -6,18 +6,9 @@ import User from "./User";
 // import Table from "react-bootstrap/Table";
 
 function App() {
-  let users = [
-    { name: "anil", email: "anil@test.com", contact: "111" },
-    { name: "neha", email: "neha@test.com", contact: "138" },
-    { name: "kapil", email: "kapil@test.com", contact: "211" },
-    { name: "anjali", email: "anjali@test.com", contact: "222" },
-  ];
   return (
     <div className="App">
-      <h1>Reuse Component with list</h1>
-      {users.map((data, a) => (
-        <User key={a} item={data} />
-      ))}
+      <h1>Lifting State Up</h1>
     </div>
   );
 }
