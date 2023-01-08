1286b69a4fee9aba830f85cc96dfdb280e402ea4
Mohit Rautela <mrautela12@gmail.com>
2023-01-09 03:32:55 +0530

    some commit

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,27 +1,43 @@
-import React from "react";
+import React, { useState, useMemo } from "react";
 import "./App.css";
 import "./Style.css";
 
 // import Table from "react-bootstrap/Table";
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      count: 1,
-    };
-  }
-  render() {
-    console.warn("check re-rendering");
-    return (
-      <div className="App">
-        <h1>Pure Component in React {this.state.count}</h1>
-        <button onClick={() => this.setState({ count: this.state.count + 1 })}>
-          click
-        </button>
-      </div>
-    );
-  }
+function App() {
+  let [count, setCount] = useState(0);
+  let [item, setItem] = useState(10);
+
+  let multiCountMemo = useMemo(
+    function multiCount() {
+      console.warn("multi-count");
+      return count * 5;
+    },
+    [count]
+  );
+  return (
+    <div className="App">
+      <h1>Use Memo Hook in React</h1>
+      <h2>Count:{count}</h2>
+      <h2>Item:{item}</h2>
+      <h2> {multiCountMemo}</h2>
+
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Update Count
+      </button>
+      <button
+        onClick={() => {
+          setItem(item * 10);
+        }}
+      >
+        Update Item
+      </button>
+    </div>
+  );
 }
 
 export default App;
