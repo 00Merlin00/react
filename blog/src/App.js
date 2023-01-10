2757288428b82e0a0fbfacf6e3f27ee98ed67f9d
Mohit Rautela <mrautela12@gmail.com>
2023-01-11 03:22:21 +0530

    some commit

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,21 +1,13 @@
-import React, { useRef } from "react";
+import React from "react";
 import "./App.css";
 import "./Style.css";
 
 // import Table from "react-bootstrap/Table";
 
 function App() {
-  let inputRef = useRef(null);
-  function handleInput() {
-    console.warn("func called");
-    inputRef.current.value = "1000";
-    inputRef.current.focus();
-  }
   return (
     <div className="App">
-      <h1>useRef in React</h1>
-      <input type={"text"} ref={inputRef}></input>
-      <button onClick={() => handleInput()}>click</button>
+      <h1>forwardRef in React</h1>
     </div>
   );
 }
