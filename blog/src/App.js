d06b2d24568df885d3eca524f3b70f020a67a659
Mohit Rautela <mrautela12@gmail.com>
2023-01-10 23:47:10 +0530

    some commit

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,41 +1,20 @@
-import React, { useState, useMemo } from "react";
+import React, { useRef } from "react";
 import "./App.css";
 import "./Style.css";
 
 // import Table from "react-bootstrap/Table";
 
 function App() {
-  let [count, setCount] = useState(0);
-  let [item, setItem] = useState(10);
-
-  let multiCountMemo = useMemo(
-    function multiCount() {
-      console.warn("multi-count");
-      return count * 5;
-    },
-    [count]
-  );
+  let inputRef = useRef(null);
+  function handleInput() {
+    console.warn("func called");
+    inputRef.current.value = "1000";
+  }
   return (
     <div className="App">
-      <h1>Use Memo Hook in React</h1>
-      <h2>Count:{count}</h2>
-      <h2>Item:{item}</h2>
-      <h2> {multiCountMemo}</h2>
-
-      <button
-        onClick={() => {
-          setCount(count + 1);
-        }}
-      >
-        Update Count
-      </button>
-      <button
-        onClick={() => {
-          setItem(item * 10);
-        }}
-      >
-        Update Item
-      </button>
+      <h1>useRef in React</h1>
+      <input type={"text"} ref={inputRef}></input>
+      <button onClick={() => handleInput()}>click</button>
     </div>
   );
 }
