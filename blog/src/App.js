33e816b29622b80946b05458f0821fef035d1126
Mohit Rautela <mrautela12@gmail.com>
2023-01-14 13:06:52 +0530

    some commit

--- blog/src/App.js
+++ blog/src/App.js
@@ -5,21 +5,7 @@ import "./Style.css";
 // import Table from "react-bootstrap/Table";
 
 function App() {
-  let [val, setVal] = useState("");
-  function submitForm(e) {
-    e.preventDefault();
-  }
-
-  return (
-    <div className="App">
-      <h1>Uncontrolled Component</h1>
-      <form onSubmit={submitForm}>
-        <input type={"text"}></input> <br /> <br />
-        <input type={"text"}></input> <br /> <br />
-      </form>
-      <button>Submit</button>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
