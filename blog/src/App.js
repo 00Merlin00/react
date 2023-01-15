a38d3659d37c67c7b4d0d6fc7dfa5b2e8ac3b61b
Mohit Rautela <mrautela12@gmail.com>
2023-01-15 17:32:52 +0530

    some commit

--- blog/src/App.js
+++ blog/src/App.js
@@ -5,7 +5,28 @@ import "./Style.css";
 // import Table from "react-bootstrap/Table";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>HOC</h1>
+      <HOC cnt={Counter} />
+    </div>
+  );
+}
+function HOC(props) {
+  return (
+    <h3>
+      <props.cnt />
+    </h3>
+  );
 }
 
+function Counter() {
+  let [count, setCount] = useState(0);
+  return (
+    <div>
+      <h3>{count}</h3>
+      <button onClick={() => setCount(count + 1)}>Update</button>
+    </div>
+  );
+}
 export default App;
