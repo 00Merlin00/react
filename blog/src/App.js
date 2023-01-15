a44086f1d8aea15725908c868f25ce4b6deef740
Mohit Rautela <mrautela12@gmail.com>
2023-01-15 18:36:03 +0530

    some commit

--- blog/src/App.js
+++ blog/src/App.js
@@ -8,25 +8,8 @@ function App() {
   return (
     <div className="App">
       <h1>HOC</h1>
-      <HOC cnt={Counter} />
     </div>
-  );
-}
-function HOC(props) {
-  return (
-    <h3>
-      <props.cnt />
-    </h3>
   );
 }
 
-function Counter() {
-  let [count, setCount] = useState(0);
-  return (
-    <div>
-      <h3>{count}</h3>
-      <button onClick={() => setCount(count + 1)}>Update</button>
-    </div>
-  );
-}
 export default App;
