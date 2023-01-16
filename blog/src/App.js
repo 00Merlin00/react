b903d9da5b444fcf173a1c7aca4c8c5ac81ca634
Mohit Rautela <mrautela12@gmail.com>
2023-01-16 22:40:23 +0530

    some commit

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,15 +1,41 @@
 import React, { useState } from "react";
 import "./App.css";
 import "./Style.css";
+import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
 
 // import Table from "react-bootstrap/Table";
 
 function App() {
   return (
     <div className="App">
-      <h1>HOC</h1>
+      <Router>
+        <Link to="./home">Home Page</Link>
+        <br />
+        <Link to="./about">About Page</Link>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
 
+function Home() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>This is Home Page of Website</p>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>This is About Page of Website</p>
+    </div>
+  );
+}
 export default App;
