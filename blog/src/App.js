d76499ddb8115ae5306e02af2e6192cc72be5e83
Mohit Rautela <mrautela12@gmail.com>
2023-01-19 00:25:37 +0530

    some commit

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,23 +1,22 @@
 import React, { useState } from "react";
 import "./App.css";
 import "./Style.css";
-import { Link, Route, Routes } from "react-router-dom";
-import Nav from "./Nav";
-import Home from "./Home";
-import About from "./About";
-
-// import Table from "react-bootstrap/Table";
 
 function App() {
+  let users = [
+    { id: 1, name: "anil", email: "anil@test.com" },
+    { id: 2, name: "megha", email: "megha@test.com" },
+    { id: 3, name: "jay", email: "jay@test.com" },
+    { id: 4, name: "kris", email: "kris@test.com" },
+    {},
+  ];
   return (
     <div className="App">
-      <Nav />
-      <Routes>
-        <Route path="/about" element={<About />} />
-        <Route path="/" exact={true} element={<Home />} />
-      </Routes>
+      <h1>React Dynamic Routing</h1>
+      {users.map((item) => (
+        <div>{item.name}</div>
+      ))}
     </div>
   );
 }
-
 export default App;
