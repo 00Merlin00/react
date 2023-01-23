5216f97d17d2cfcee13987af463b3db70164f212
Mohit Rautela <mrautela12@gmail.com>
2023-01-24 00:04:57 +0530

    some commit

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,30 +1,12 @@
 import React from "react";
 import "./App.css";
 import "./Style.css";
-import { Link, Routes, Route } from "react-router-dom";
-import User from "./User";
 
 function App() {
-  let users = [
-    { id: 1, name: "anil", email: "anil@test.com" },
-    { id: 2, name: "megha", email: "megha@test.com" },
-    { id: 3, name: "jay", email: "jay@test.com" },
-    { id: 4, name: "kris", email: "kris@test.com" },
-    { id: 5, name: "mridul", email: "mridul@test.com" },
-  ];
+  fetch("url");
   return (
     <div className="App">
       <h1>React Dynamic Routing</h1>
-      {users.map((item, k) => (
-        <div key={k}>
-          <Link to={`/user/${item.id}`} state={item}>
-            <h3>{item.name}</h3>
-          </Link>
-        </div>
-      ))}
-      <Routes>
-        <Route path="/user/:name" element={<User />} />
-      </Routes>
     </div>
   );
 }
