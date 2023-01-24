079eb89d116d45ad9e2d6186d125e4370591b6fd
Mohit Rautela <mrautela12@gmail.com>
2023-01-25 00:32:07 +0530

    some commit

--- blog/src/App.js
+++ blog/src/App.js
@@ -7,6 +7,10 @@ function App() {
   return (
     <div className="App">
       <h1>Post Api</h1>
+      <input type={"text"} name="name"></input> <br /> <br />
+      <input type={"text"} name="name"></input> <br /> <br />
+      <input type={"text"} name="name"></input> <br /> <br />
+      <button>Save New User</button>
     </div>
   );
 }
