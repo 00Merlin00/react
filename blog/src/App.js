5e6a633153148c38cabd5e498e072858ea7888bb
Mohit Rautela <mrautela12@gmail.com>
2023-01-26 01:16:02 +0530

    some commit

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,16 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import "./Style.css";
 
 function App() {
-  fetch("url");
+  let [name, setName] = useState("");
+  let [email, setEmail] = useState("");
+  let [mobile, setMobile] = useState("");
+
+  function saveUser() {
+    console.warn(name, email, mobile);
+  }
   return (
     <div className="App">
       <h1>Post Api</h1>
-      <input type={"text"} name="name"></input> <br /> <br />
-      <input type={"text"} name="name"></input> <br /> <br />
-      <input type={"text"} name="name"></input> <br /> <br />
-      <button>Save New User</button>
+      <input
+        type={"text"}
+        value={name}
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+        name="name"
+      ></input>
+      <br /> <br />
+      <input
+        type={"text"}
+        value={email}
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+        name="email"
+      ></input>
+      <br /> <br />
+      <input
+        type={"text"}
+        value={mobile}
+        onChange={(e) => {
+          setMobile(e.target.value);
+        }}
+        name="mobile"
+      ></input>
+      <br /> <br />
+      <button onClick={saveUser}>Save New User</button>
     </div>
   );
 }
