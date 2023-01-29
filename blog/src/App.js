f1410f955f0457f30f53d7cb1d7431ddc4d08d11
Mohit Rautela <mrautela12@gmail.com>
2023-01-30 01:09:22 +0530

    some commit

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,45 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import "./Style.css";
 
 function App() {
-  let [name, setName] = useState("");
-  let [email, setEmail] = useState("");
-  let [mobile, setMobile] = useState("");
+  let [count, setCount] = useState(1);
 
-  function saveUser() {
-    console.warn(name, email, mobile);
-  }
   return (
     <div className="App">
-      <h1>Post Api</h1>
-      <input
-        type={"text"}
-        value={name}
-        onChange={(e) => {
-          setName(e.target.value);
-        }}
-        name="name"
-      ></input>
-      <br /> <br />
-      <input
-        type={"text"}
-        value={email}
-        onChange={(e) => {
-          setEmail(e.target.value);
-        }}
-        name="email"
-      ></input>
-      <br /> <br />
-      <input
-        type={"text"}
-        value={mobile}
-        onChange={(e) => {
-          setMobile(e.target.value);
-        }}
-        name="mobile"
-      ></input>
-      <button onClick={saveUser}>Save New User</button>
+      <h1>Previous state {count}</h1>
+
+      <button>Update</button>
     </div>
   );
 }
