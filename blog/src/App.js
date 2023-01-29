24e42a7e444003a720f05f2bf2b18beb093cfbe2
Mohit Rautela <mrautela12@gmail.com>
2023-01-30 01:28:25 +0530

    some commit

--- blog/src/App.js
+++ blog/src/App.js
@@ -4,12 +4,17 @@ import "./Style.css";
 
 function App() {
   let [count, setCount] = useState(1);
+  function updateCounter() {
+    for (let i = 0; i < 5; i++) {
+      setCount((pre) => pre + 1);
+    }
+  }
 
   return (
     <div className="App">
       <h1>Previous state {count}</h1>
 
-      <button>Update</button>
+      <button onClick={updateCounter}>Update Counter</button>
     </div>
   );
 }
