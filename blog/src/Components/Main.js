f878a34a5394eb84a5a2d63589fbe4435d11e575
Mohit Rautela <mrautela12@gmail.com>
2023-02-06 02:49:22 +0530

    some commit

--- blog/src/Components/Main.js
+++ blog/src/Components/Main.js
@@ -4,7 +4,7 @@ import { CommonContext } from "./CommonContext";
 function Main() {
   return (
     <CommonContext.Consumer>
-      {({ theme }) => <h1>Hllo this is main page.</h1>}
+      {({ theme z}) => <h1>Hllo this is main page.</h1>}
     </CommonContext.Consumer>
   );
 }
